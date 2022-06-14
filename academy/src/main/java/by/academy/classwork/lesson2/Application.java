f15d0254d4e32b7d86a99f05c7514d63499ba8e2
PolinaLuchenok <welestor.opt@gmail.com>
2022-06-14 21:30:34 +0300

    Mur

--- academy/src/main/java/by/academy/classwork/lesson2/Application.java
+++ academy/src/main/java/by/academy/classwork/lesson2/Application.java
@@ -3,7 +3,7 @@ package by.academy.classwork.lesson2;
 public class Application {
 	public static void main(String[] args) {
 		Cat cat1 = new Cat();
-		Cat cat2 = new Cat("Murz");
+		Cat cat2 = new Cat("Mur");
 
 		cat2.eat();
 		cat2.walk();
